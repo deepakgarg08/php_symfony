dfd704c3adad89bb2186f486fe6e8fe048d18db3
Deepak <erdkgarg.08@gmail.com>
2021-04-16 17:44:41 +0200

    entities executed successfully for previous work see this https://github.com/deepakgarg08/php_symfony

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,35 +9,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default/", name="default")
+     * @Route("/", name="default")
      */
 
-    //     * @Route("/default/{name}", name="default")
-
-    public function index(/*$name*/)
+ 
+    public function index()
     {
-//        return $this->render('default/index.html.twig', [
-//            'controller_name' => 'DefaultController',
-//        ]);
-//        return $this->json(['username' => 'Deepak']);
-//        return $this->json(['username' => 'Deepak']);
-//        return new Response('Hello  ' .  $name);
-//        return $this->redirect("https://www.google.com");
 
-         return $this->redirectToRoute("default2");
-//        return new Response('<b>Hello Deepak</b>');
+       return new Response('<b>Hello Deepak</b>');
 
     }
 
-    /**
-     * @Route("/default2/", name="default2")
-     */
-    public function index2()
 
-    {
-
-//        return new Response('<b>Hello Deepak from default2</b>');
-
-    }
 
 }
